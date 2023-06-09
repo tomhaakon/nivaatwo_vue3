60953bdab3885c1b2a73eca7dfdd0eacb0a088d2
Tom Fjeldheim <tom.fjeldheim@googlemail.com>
2023-06-09 07:47:20 +0200

    testing

--- src/views/ResourcesView.vue
+++ src/views/ResourcesView.vue
@@ -1,76 +1,82 @@
 <template>
-  <!-- root element -->
-  <p>import ResourcesView.vue OK</p>
-  <Table
-    :showModal="showModal"
-    :blabla="blabla"
-    @closeModal="showModal = !showModal"
-  ></Table>
-
-  <!-- tabell -->
-  <div class="p-20">
-    <table class="bg-neutral-100 w-full">
+  <div>
+    <input type="text" v-model="searchQuery" placeholder="Search..." />
+    <table>
       <thead>
-        <tr class="shadow-md text-left">
-          <th class="w-1/4" @click="sortData('title')">Title &#8597;</th>
-          <th class="w-1/4" @click="sortData('description')">
-            Description &#8597;
-          </th>
-          <th class="w-1/4" @click="sortData('link')">Link &#8597;</th>
-          <th>
-            <!-- søk input -->
-            <div>
-              <input
-                type="search"
-                placeholder="trykk her for å søke"
-                v-model="search"
-                class="bg-neutral-100 p-2"
-              />
-              <div
-                v-for="entry in (filteredData, sortedData)"
-                :key="entry.id"
-              ></div>
-            </div>
-            <!--  slutt søk input -->
-          </th>
+        <tr>
+          <th @click="sort('name')">Name</th>
+          <th @click="sort('age')">Age</th>
+          <th @click="sort('email')">Email</th>
         </tr>
       </thead>
-      <tbody v-for="car in filteredData">
-        <tr>
-          <td>{{ car.title }}</td>
-          <td>{{ car.description }}</td>
-          <td>{{ car.link }}</td>
-          <td>knapp</td>
+      <tbody>
+        <tr v-for="item in filteredItems" :key="item.id">
+          <td>{{ item.name }}</td>
+          <td>{{ item.age }}</td>
+          <td>{{ item.email }}</td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
-<script setup>
-import { ref, computed } from "vue";
-import Table from "@/components/Table.vue";
 
-// import data
-import jsonData from "@/json/cars.json";
+<script>
+import { reactive, toRefs, computed } from "vue";
 
-const myData = ref(jsonData);
+export default {
+  setup() {
+    const data = reactive({
+      items: [
+        { id: 1, name: "John Doe", age: 25, email: "john@example.com" },
+        { id: 2, name: "Jane Smith", age: 30, email: "jane@example.com" },
+        { id: 3, name: "Bob Johnson", age: 35, email: "bob@example.com" },
+      ],
+      sortBy: "",
+      sortDirection: "asc",
+      searchQuery: "",
+    });
 
-// modal
-const showModal = ref(false);
-const openModal = (item) => {
-  blabla.value = item;
-  showModal.value = true;
+    const sort = (column) => {
+      if (data.sortBy === column) {
+        data.sortDirection = data.sortDirection === "asc" ? "desc" : "asc";
+      } else {
+        data.sortBy = column;
+        data.sortDirection = "asc";
+      }
+    };
+
+    const filteredItems = computed(() => {
+      let items = [...data.items];
+      if (data.searchQuery) {
+        items = items.filter((item) => {
+          const searchValue = data.searchQuery.toLowerCase();
+          return (
+            item.name.toLowerCase().includes(searchValue) ||
+            item.age.toString().includes(searchValue) ||
+            item.email.toLowerCase().includes(searchValue)
+          );
+        });
+      }
+      if (data.sortBy) {
+        items.sort((a, b) => {
+          const fieldA = a[data.sortBy];
+          const fieldB = b[data.sortBy];
+
+          if (data.sortDirection === "asc") {
+            return fieldA.localeCompare(fieldB);
+          } else {
+            return fieldB.localeCompare(fieldA);
+          }
+        });
+      }
+      return items;
+    });
+
+    return {
+      ...toRefs(data),
+      sort,
+      filteredItems,
+    };
+  },
 };
-const blabla = ref("");
-
-// search
-const search = ref("");
-const filteredData = computed(() => {
-  return myData.value.filter((car) => {
-    const searchData = car.title + car.description + car.link;
-    return searchData.toLowerCase().includes(search.value.toLowerCase());
-  });
-});
-
-// sortering
 </script>
